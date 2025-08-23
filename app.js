8c4cb95789b274602a8c72ee53d0bb6d805f94a7
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-08-23 18:53:33 +0300

    another test

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
+// app.get('/health', (req, res) => {
+//   res.send('ok')
+// })
+
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw 'error...  '
   res.send('ok')
 })
 
