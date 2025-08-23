28a9fd47e45085133d46ff78cd2c4bd3ac77eebf
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-08-23 13:27:56 +0300

    e2e test

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -13,4 +13,14 @@ describe('Pokedex', () => {
       )
     ).toBeVisible()
   })
+
+  test('navigate to ivysaur page and check ability', async () => {
+    const browser = await chromium.launch({ channel: 'chrome' })
+    const page = await browser.newPage()
+
+    await page.goto('http://localhost:5001/')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+    await browser.close()
+  })
 })
